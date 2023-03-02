2dc041d20200b6f1c19ff041b622b8be51644d80
Dor Moyal <moyal17@gmail.com>
2023-03-02 17:16:10 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {useState} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container, Row, Col } from 'react-bootstrap';
 import Input from './Components/Input'
@@ -5,12 +6,16 @@ import AnswerBlock from './Components/AnswerBlock'
 import './App.css';
 
 function App() {
+  const handleQuestion = async (value) => {
+
+  }
+
   return (
     <div className="App">
       <Container>
         <Row className="text-center">
             <Col lg={12}>
-              <Input/>
+              <Input handleQuestion={(value) => handleQuestion(value)}/>
             </Col>
             <Col lg={12}>
               <AnswerBlock/>
--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,16 +1,23 @@
 import { InputGroup, Form} from 'react-bootstrap';
 import apiService from "../services/apiService";
+import {useState} from "react";
 
 
-function Input() {
+function Input(props) {
   apiService.askQuestion({question: ''})
+  const [question, setQuestion] = useState(null)
+
+  const setValue = (e) => {
+    console.log(e.target.value);
+    setQuestion(e.target.value)
+  }
 
   return (
     <InputGroup size="lg" className="mb-3">
       <InputGroup.Text id="basic-addon3">
         Ask Me Something
       </InputGroup.Text>
-      <Form.Control id="basic-url" aria-describedby="basic-addon3" />
+      <Form.Control id="basic-url" aria-describedby="basic-addon3"  onChange={(e) => { setValue(e)}}/>
     </InputGroup>
   );
 }
